06c38a077c72c5f862490832ee84583f39985c9b
Kyle Bock <kylewbock@gmail.com>
2012-09-11 15:29:02 -0400

    fixed sidebar and loop. loop is semi styled and sidebar widget titles are styled similar to panels on eul web

--- index.php
+++ index.php
@@ -9,7 +9,28 @@
  */
 ?>
 <?php get_header(); ?>
-        <div id="main_content" role="main">
-            main content
+        <div id="content">
+            <div id="main" role="main">
+                <!-- The Loop -->
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                <article class="post-<?php the_ID(); ?> post" role="article">
+                    <header class="post-header">
+                        <h1><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+                        <p class="meta"><?php _e('Posted', 'bonestheme'); ?> <time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time(get_option('date_format')); ?></time> <?php _e('by', 'bonestheme'); ?> <?php the_author_posts_link(); ?> <span class="amp">&</span> <?php _e('filed under', 'bonestheme'); ?> <?php the_category(', '); ?>.</p>
+                    </header><!-- /.post-header -->
+                    <section class="post-content">
+                        <?php the_content(); ?>
+                    </section><!-- /.post-content -->
+                    <footer class="post-footer">
+                        <p class="tags"><?php the_tags('<span class="tags-title">Tags:</span> ', ', ', ''); ?></p>
+                    </footer>
+                </article>
+                <?php endwhile; else: ?>
+
+                <?php endif; ?>
+                <!-- /The Loop -->
+            </div><!-- /#main -->
+            <?php get_sidebar(); ?>
+            <div class="clearfix"></div>
         </div>
 <?php get_footer(); ?>
--- sidebar.php
+++ sidebar.php
@@ -7,6 +7,17 @@
 
 ?>
 
-<div id="secondary" class="widget-area" role="complementary">
-    <?php dynamic_sidebar('sidebar-1'); ?>
+<div id="sidebar" class="widget-area" role="complementary">
+    <?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+
+        <?php dynamic_sidebar( 'sidebar-1' ); ?>
+
+    <?php else : ?>
+
+        <!-- This content shows up if there are no widgets defined in the backend. -->
+        <div class="alert help">
+            <p>Please activate some Widgets.</p>
+        </div>
+
+    <?php endif; ?>
 </div>

50c31c77a3397b7740b1ce48a10695551758fbca
Kyle Bock <kylewbock@gmail.com>
2012-09-20 16:05:23 -0400

    moving things around, globaly p tag style

--- functions.php
+++ functions.php
@@ -78,7 +78,7 @@ function eul_comment( $comment, $args, $depth ) {
 
     <article id="comment-<?php comment_ID(); ?>" class="comment">
       <div class="comment-meta">
-        
+        <?php echo get_avatar( $comment, $args['avatar_size'] ); ?>
       </div>
       <div class="comment-content"><?php comment_author_link(); ?> <?php comment_text(); ?></div>
       <footer>

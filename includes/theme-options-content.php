a93eb1fdd6696325fa69dcf0693ac7e77db6a00d
Kyle Bock <kylewbock@gmail.com>
2012-09-25 13:41:44 -0400

    options page header

--- includes/theme-options-content.php
+++ includes/theme-options-content.php
@@ -4,3 +4,9 @@
  * The content to be rendered for the theme options page
  */
 ?>
+<div class="wrap">
+    <div id="icon-themes" class="icon32">
+        <br />
+    </div>
+    <h2><?php _e('Theme Options', 'eultheme'); ?></h2>
+</div>

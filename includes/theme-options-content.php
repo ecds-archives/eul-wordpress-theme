80cdd97067bd4bae21c6fce6b0dca84b6071ba4e
Kyle Bock <kylewbock@gmail.com>
2012-09-28 15:19:34 -0400

    adding theme optiosn
    + working dynamic theme optiosn page driven out of associative array
    + need to add the defaults population and make the restore defaults
    work correctly

--- includes/theme-options-content.php
+++ includes/theme-options-content.php
@@ -9,4 +9,12 @@
         <br />
     </div>
     <h2><?php _e('Theme Options', 'eultheme'); ?></h2>
-</div>
+    <form action="options.php" method="post">
+        <?php
+            settings_fields('eul_theme_options');
+            do_settings_sections('theme_options');
+        ?>
+        <input name="eul_theme_options[submit-<?php echo $tab; ?>]" type="submit" class="button-primary" value="<?php esc_attr_e('Save Settings', 'eultheme'); ?>" />
+        <input name="eul_theme_options[reset-<?php echo $tab; ?>]" type="submit" class="button-secondary" value="<?php esc_attr_e('Reset Defaults', 'eultheme'); ?>" />
+    </form>
+</div>  
--- includes/theme-options.php
+++ includes/theme-options.php
@@ -4,18 +4,77 @@
  * @package EULTheme
  * @since EULTheme 0.3.0
  */
+global $sections;
+$sections = array(
+    'header' => array(
+        'id' => 'header',
+        'title' => __('Header', 'eultheme'),
+        'description' => __('Options related to the header of the page')
+    )
+);
 
+global $options;
+$options = array(
+    'header_site_title' => array(
+        'id' => 'header_site_title',
+        'title' => __('Header Title', 'eultheme'),
+        'description' => 'The title of the site next to the Emory Logo',
+        'section' => 'header',
+        'type' => 'text',
+        'default' => ''
+    ),
+    'header_nav_color' => array(
+        'id' => 'header_nav_color',
+        'title' => __('Navigation Color', 'eultheme'),
+        'description' => 'The background color of the header navigation bar',
+        'section' => 'header',
+        'type' => 'select',
+        'valid_options' => array(
+            'gold' => array(
+                'value' => 'gold',
+                'title' => 'Gold'
+            ),
+            'grey' => array(
+                'value' => 'grey',
+                'title' => 'Grey'
+            )
+        ),
+        'default' => 'gold'
+    )
+);
 
 function eul_theme_options_init() {
     register_setting(
-        'eul_options',       // Options group, see settings_fields() call in twentyeleven_theme_options_render_page()
+        'eul_theme_options',       // Options group, see settings_fields() call in twentyeleven_theme_options_render_page()
         'eul_theme_options', // Database option, see twentyeleven_get_theme_options()
         'eul_theme_options_validate' // The sanitization callback, see twentyeleven_theme_options_validate()
     );
+    
+    global $sections;
+    foreach ($sections as $section) {
+        add_settings_section(
+            $section['id'], 
+            $section['title'], 
+            'eul_theme_sections_callback', 
+            'theme_options');
+    }
+
+    global $options;
+    foreach ($options as $option) {
+        add_settings_field(
+            $option['id'], 
+            $option['title'], 
+            'eul_theme_options_callback', 
+            'theme_options', 
+            $option['section'],
+            $option
+        );
+    }
 }
 add_action( 'admin_init', 'eul_theme_options_init' );
 
 function eul_theme_options_add_menu() {
+    // responsible for actually creating the settings page and adding it to the appropriate menu
     add_theme_page(
         __( 'Theme Options', 'eultheme' ),      // Name of page
         __( 'Theme Options', 'eultheme' ),      // Label in menu
@@ -27,11 +86,56 @@ function eul_theme_options_add_menu() {
 add_action( 'admin_menu', 'eul_theme_options_add_menu' );
 
 function eul_theme_options_render_page() {
+    // early return for failed authorization
     if ( !current_user_can( 'manage_options' ) )  {
         wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
     }
+
     // done to keep this function from getting unwieldy
     include("theme-options-content.php");
+}
+
+function eul_theme_sections_callback($section) {
+    global $sections;
+    echo "<p>" . $sections[$section['id']]['description'] . "</p>";
+}
+
+function eul_theme_options_callback($option) {
+    // $oenology_options = oenology_get_options();
+    // $option_parameters = oenology_get_option_parameters();
+    $optionname = $option['name'];
+    $optiontitle = $option['title'];
+    $optiondescription = $option['description'];
+    $fieldtype = $option['type'];
+    $fieldname = 'eul_theme_options[' . $optionname . ']';
+    
+    // build out code for custom option types here
+    // Output select form field markup
+    if ( 'text' == $fieldtype) {
+        // for value echo wp_filter_nohtml_kses( $oenology_options[$optionname] ); 
+        ?>
+        <input type="text" name="<?php echo $fieldname; ?>" value="" />
+        <?php
+    }
+    else if ( 'select' == $fieldtype ) {
+        $valid_options = array();
+        $valid_options = $option['valid_options'];
+        ?>
+        <select name="<?php echo $fieldname; ?>">
+        <?php 
+        foreach ( $valid_options as $valid_option ) {
+            echo $valid_option;?>
+            <option value="<?php echo $valid_option['name']; ?>"><?php echo $valid_option['title']; ?></option>
+            <?php
+        }
+        ?>
+        </select>
+        <?php
+    } 
+}
+
+function pre($array) {
+    echo "<pre>" . print_r($array, true) . "</pre>";
 }
 
 ?>

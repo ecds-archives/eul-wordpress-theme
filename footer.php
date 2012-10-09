e4c38f8527dc0ce966941aa1081468c0c99a4565
Kyle Bock <kylewbock@gmail.com>
2012-10-09 11:01:46 -0400

    styled footer and added copyright notice

--- footer.php
+++ footer.php
@@ -8,8 +8,16 @@
             <!-- <div id="share_bar">
 
             </div> -->
-            <div id="links">
-
+            <div id="links" role="navigation">
+                <?php 
+                    wp_nav_menu(array(
+                        'container'         => false,
+                        'theme_location'    => 'primary'
+                    )); 
+                ?>
+                <div id="copyright">
+                    Copyright © 2012 Emory University - All Rights Reserved | 201 Dowman Drive, Atlanta, Georgia 30322 USA 404.727.6123
+                </div>
             </div>
         </footer>
     </div><!-- /#page_wrapper -->
